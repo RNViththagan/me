6f1285056185fa9ff39fa76095ac6d53e5ec9370
Viththagan <viththagan1999@gmail.com>
2025-02-03 23:34:33 +0530

    Fix GitHub Pages deployment

--- components/hero.tsx
+++ components/hero.tsx
@@ -15,7 +15,7 @@ export default function Hero() {
         </div>
         <div className="md:w-1/2 flex justify-center">
           <div className="relative w-64 h-64 rounded-full overflow-hidden border-4 border-white shadow-lg animate-float">
-            <Image   style={{ objectPosition: "bottom" }}  src="/Viththagan.jpeg" alt="Viththagan" layout="fill" objectFit="cover" />
+            <Image   style={{ objectPosition: "bottom" }}  src="./Viththagan.jpeg" alt="Viththagan" layout="fill" objectFit="cover" />
           </div>
         </div>
       </div>
