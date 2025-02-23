80aa1415c69bbee952bf410c0b9db02c7e8c7d4c
Viththagan <viththagan1999@gmail.com>
2025-02-23 11:42:58 +0530

    add margin in video section

--- components/video-portfolio.tsx
+++ components/video-portfolio.tsx
@@ -194,7 +194,7 @@ export default function VideoPortfolio() {
             <LayoutGroup>
               <motion.div
                 layout
-                className={`grid gap-6 ${
+                className={`grid gap-6 md:m-5  ${
                   isExpanded
                     ? "grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3"
                     : "grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
